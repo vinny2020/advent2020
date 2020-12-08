bf5d5c95a06fc7715e93bb48dbe923a7b933457a
Vincent Stoessel <vincents@backbase.com>
2020-12-08 09:23:09 -0500

    split day 2, parts 1 and 2 into separate classes

--- src/main/kotlin/main0201.kt
+++ src/main/kotlin/main0201.kt
@@ -6,17 +6,16 @@ import java.io.File
       var validPasswords = 0
       File("src/main/resources/day02/puzzle-day2-input.txt").forEachLine {
          //println(it)
-         val tokensday2 = it.split(" ")
-         val ruleSet = tokensday2[0]
-         val needle = tokensday2[1].split(":")[0].toCharArray(startIndex = 0, endIndex = 1)
+         val tokens = it.split(" ")
+         val ruleSet = tokens[0]
+         val needle = tokens[1].split(":")[0].toCharArray(startIndex = 0, endIndex = 1)
          val needleCh :Char  = needle[0]
-         val haystack = tokensday2[2]
-         val firstValid = ruleSet.split("-")[0].toInt()
-         val secondValid = ruleSet.split("-")[1].toInt()
-         println("First position: $firstValid second position: $secondValid for this char $needleCh for this text $haystack")
-         if( haystack[firstValid -1 ] == needleCh && haystack[secondValid -1 ] != needleCh
-            ||
-            haystack[secondValid -1 ] == needleCh && haystack[firstValid -1 ] != needleCh    ) {
+         val haystack = tokens[2]
+         val minOccurrence = ruleSet.split("-")[0].toInt()
+         val maxOccurrence = ruleSet.split("-")[1].toInt()
+         val count = haystack.filter { it == needleCh }.count()
+         println("Rule Min: $minOccurrence Rule Max: $maxOccurrence for this char $needleCh for this text $haystack, count $count")
+         if(count in minOccurrence..maxOccurrence) {
             validPasswords++
          }
       }
